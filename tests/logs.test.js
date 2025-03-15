b042a7feb4170b7efbc246a259decb2ed924edc1
Ryan Christian <rchristian@ryanchristian.dev>
2025-03-14 21:11:07 -0500

    test: Add cases for stripping & preserving sourcemap size

--- tests/logs.test.js
+++ tests/logs.test.js
@@ -2,6 +2,9 @@ import { test } from 'uvu';
 import * as assert from 'uvu/assert';
 
 import { setupTest, teardownTest, loadFixture, viteBuildCli } from './lib/lifecycle.js';
+import { writeFixtureFile } from './lib/utils.js';
+
+const writeConfig = async (dir, content) => writeFixtureFile(dir, 'vite.config.js', content);
 
 let env;
 test.before.each(async () => {
@@ -12,19 +15,45 @@ test.after.each(async () => {
     await teardownTest(env);
 });
 
-test('Should support the `prerenderScript` plugin option', async () => {
+test('Should log which routes were prerendered & where they were discovered', async () => {
     await loadFixture('logs/prerendered-routes', env);
     const output = await viteBuildCli(env.tmp.path);
     await output.done;
 
-    const idx = output.stdout.findIndex((line) => line.includes('Prerendered'));
     // The prerender info is pushed as a single log line
-    const stdout = output.stdout.slice(idx)[0];
+    const stdout = output.stdout.find((line) => line.includes('Prerendered'));
 
     assert.match(stdout, 'Prerendered 3 pages:\n');
     assert.match(stdout, '/\n');
     assert.match(stdout, '/foo [from /]\n');
     assert.match(stdout, '/bar [from /foo]\n');
+});
+
+test('Should strip sourcemap sizes from logs if user has not enabled sourcemaps', async () => {
+    await loadFixture('logs/prerendered-routes', env);
+    const output = await viteBuildCli(env.tmp.path);
+    await output.done;
+
+    const stdout = output.stdout.find((line) => /dist\/assets\/index.*\.js/.test(line));
+    assert.not.match(stdout, '│ map:');
+});
+
+test('Should preserve sourcemap sizes from logs if user has enabled sourcemaps', async () => {
+    await loadFixture('logs/prerendered-routes', env);
+    await writeConfig(env.tmp.path, `
+        import { defineConfig } from 'vite';
+        import { vitePrerenderPlugin } from 'vite-prerender-plugin';
+
+        export default defineConfig({
+            build: { sourcemap: true },
+            plugins: [vitePrerenderPlugin()],
+        });
+    `);
+    const output = await viteBuildCli(env.tmp.path);
+    await output.done;
+
+    const stdout = output.stdout.find((line) => /dist\/assets\/index.*\.js/.test(line));
+    assert.match(stdout, '│ map:');
 });
 
 test.run();
